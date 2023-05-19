6b86e9c5117da751b20627591bceaa12ecc46871
Jesse Slater <jsssltr@gmail.com>
2023-05-19 12:55:17 -0500

    Seperatated out parsing logic in preparation to add more modes.

--- src/main.rs
+++ src/main.rs
@@ -7,33 +7,58 @@ fn main() {
     println!("Use ^c to quit.");
     println!("Type the letter corresponding to the given number.");
 
+    let input_instruction = "Input a single lowercase letter.";
     loop {
         let number = rand::thread_rng().gen_range(1..=26);
         println!("---{number}---");
         let mut input;
-        let mut letter: u8;
         loop {
             print!(">");
-            io::stdout().flush();
+            io::stdout().flush().expect("Error printing");
             input = String::new();
             io::stdin()
                 .read_line(&mut input)
                 .expect("Error reading input");
-            letter = match input.bytes().next() {
-                Some(byte) => byte,
-                None => {
-                    println!("Enter a single lowercase letter.");
-                    continue;
+            match parse_input(&input, &ALPHABET) {
+                Ok(n) => {
+                    if n + 1 == number {
+                        println!("Correct!");
+                        break;
+                    } else {
+                        println!("Incorrect.");
+                    }
                 }
-            };
-            if letter < 97 || letter > 122 {
-                println!("Enter a single lowercase letter.");
-                continue;
-            };
-            if letter - 96 == number {
-                println!("Correct");
-                break;
-            };
+                Err(e) => match e {
+                    ParseError::ToManyInputs => println!("To Many Inputs; {input_instruction}"),
+                    ParseError::NotInLanguage => println!("Invalid Input; {input_instruction}"),
+                },
+            }
         }
     }
+}
+
+const ALPHABET: [&str; 26] = [
+    "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s",
+    "t", "u", "v", "w", "x", "y", "z",
+];
+const ARRAY26: [&str; 26] = [
+    "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17",
+    "18", "19", "20", "21", "22", "23", "24", "25", "26",
+];
+
+#[derive(Debug)]
+enum ParseError {
+    ToManyInputs,
+    NotInLanguage,
+}
+
+fn parse_input(input: &str, base: &[&str]) -> Result<usize, ParseError> {
+    let mut input = input.clone();
+    input = input.trim();
+    if input.contains(char::is_whitespace) {
+        return Err(ParseError::ToManyInputs);
+    }
+    base.iter()
+        .position(|&s| s == input)
+        .ok_or(ParseError::NotInLanguage)
 }
