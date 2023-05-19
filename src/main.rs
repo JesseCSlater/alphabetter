3f9c7ed8f58d36f03ee7a6d214882cb3fc6b5d64
Jesse Slater <jsssltr@gmail.com>
2023-05-19 10:31:36 -0500

    Built most basic game functionallity. Only supports number to letter.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,39 @@
+use rand::Rng;
+use std::io;
+use std::io::Write;
+
 fn main() {
-    println!("Hello, world!");
-}
+    println!("Welcome to alphabetter!");
+    println!("Use ^c to quit.");
+    println!("Type the letter corresponding to the given number.");
 
-fn test() {
-    println!("test");
+    loop {
+        let number = rand::thread_rng().gen_range(1..=26);
+        println!("---{number}---");
+        let mut input;
+        let mut letter: u8;
+        loop {
+            print!(">");
+            io::stdout().flush();
+            input = String::new();
+            io::stdin()
+                .read_line(&mut input)
+                .expect("Error reading input");
+            letter = match input.bytes().next() {
+                Some(byte) => byte,
+                None => {
+                    println!("Enter a single lowercase letter.");
+                    continue;
+                }
+            };
+            if letter < 97 || letter > 122 {
+                println!("Enter a single lowercase letter.");
+                continue;
+            };
+            if letter - 96 == number {
+                println!("Correct");
+                break;
+            };
+        }
+    }
 }
-
