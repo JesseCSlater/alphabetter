e6fe92e4d9c6c6f8cc14f0ac66740633d52ec6de
Jesse Slater <jsssltr@gmail.com>
2023-05-19 14:09:50 -0500

    Moved matching game logic into seperate function, and generalized it for multiple game modes. Added second game mode.

--- src/main.rs
+++ src/main.rs
@@ -2,51 +2,37 @@ use rand::Rng;
 use std::io;
 use std::io::Write;
 
-const MODES: [&str; 1] = ["1"];
+const MODES: [&str; 2] = ["1", "2"];
 
 fn main() {
     println!("Welcome to alphabetter!");
     println!("Use ^c to quit at any time.");
-    println!("Choose your mode.");
-    println!("(1) Number to Letter");
     let mut input = String::new();
     let input_instruction = "Input the number of a mode.";
     loop {
+        println!("Choose your mode.");
+        println!("(1) Number to Letter");
+        println!("(2) Letter to Number");
         request_input(">", &mut input);
         match parse_input(&input, &MODES) {
-            Ok(_) => break,
+            Ok(0) => matching_game(
+                26,
+                &ARRAY26,
+                &ALPHABET,
+                "number from 1 to 26",
+                "lowercase letter",
+            ),
+            Ok(_) => matching_game(
+                26,
+                &ALPHABET,
+                &ARRAY26,
+                "lowercase letter",
+                "number from 1 to 26",
+            ),
             Err(e) => match e {
-                ParseError::ToManyInputs => println!("To Many Inputs; {input_instruction}"),
-                ParseError::NotInLanguage => println!("Invalid Input; {input_instruction}"),
+                ParseError::ToManyInputs => println!("To Many Inputs; {input_instruction}."),
+                ParseError::NotInLanguage => println!("Invalid Input; {input_instruction}."),
             },
-        }
-    }
-    println!("Type the letter corresponding to the given number.");
-    let input_instruction = "Input a single lowercase letter.";
-    loop {
-        let number = rand::thread_rng().gen_range(1..=26);
-        println!("---{number}---");
-        loop {
-            print!(">");
-            io::stdout().flush().expect("Error printing");
-            input = String::new();
-            io::stdin()
-                .read_line(&mut input)
-                .expect("Error reading input");
-            match parse_input(&input, &ALPHABET) {
-                Ok(n) => {
-                    if n + 1 == number {
-                        println!("Correct!");
-                        break;
-                    } else {
-                        println!("Incorrect.");
-                    }
-                }
-                Err(e) => match e {
-                    ParseError::ToManyInputs => println!("To Many Inputs; {input_instruction}"),
-                    ParseError::NotInLanguage => println!("Invalid Input; {input_instruction}"),
-                },
-            }
         }
     }
 }
@@ -66,13 +52,14 @@ enum ParseError {
     NotInLanguage,
 }
 
-fn parse_input(input: &str, base: &[&str]) -> Result<usize, ParseError> {
+fn parse_input(input: &str, options: &[&str]) -> Result<usize, ParseError> {
     let mut input = input.clone();
     input = input.trim();
     if input.contains(char::is_whitespace) {
         return Err(ParseError::ToManyInputs);
     }
-    base.iter()
+    options
+        .iter()
         .position(|&s| s == input)
         .ok_or(ParseError::NotInLanguage)
 }
@@ -82,4 +69,41 @@ fn request_input(prompt: &str, input: &mut String) {
     io::stdout().flush().expect("Error printing");
     *input = String::new();
     io::stdin().read_line(input).expect("Error reading input");
+}
+
+fn matching_game(
+    alpha_len: usize,
+    clue_alpha: &[&str],
+    guess_alpha: &[&str],
+    clue_name: &str,
+    guess_name: &str,
+) {
+    println!("Input the {guess_name} which cooresponds to the {clue_name}. How many can you get?");
+    let mut score: usize = 0;
+    let mut input = String::new();
+    'outer: loop {
+        let index = rand::thread_rng().gen_range(0..alpha_len);
+        println!("---{}---", clue_alpha[index]);
+        loop {
+            request_input(">", &mut input);
+            match parse_input(&input, guess_alpha) {
+                Ok(n) => {
+                    if n == index {
+                        println!("Correct!");
+                        score += 1;
+                        println!("Your score is {score}.");
+                        break;
+                    } else {
+                        println!("Incorrect :(");
+                        println!("Your score was {score}.");
+                        break 'outer;
+                    }
+                }
+                Err(e) => match e {
+                    ParseError::ToManyInputs => println!("To Many Inputs; Input a {guess_name}"),
+                    ParseError::NotInLanguage => println!("Invalid Input; Input a {guess_name}"),
+                },
+            }
+        }
+    }
 }
